0c1060492e5eec650c259421bf51e60292b8e292
Luke Kasson <luke.kasson@gmail.com>
2021-10-16 10:39:08 -0600

    Add project files.

---
+++ CodeWars/Rot13/Rot13.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Rot13
+{
+	public class Rot13
+	{
+		public string Method(string input)
+		{
+			return "Code Here";
+		}
+	}
+}
---
+++ CodeWars/Rot13/Rot13Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeWars.Rot13
+{
+	public class Rot13Tests
+	{
+		private Rot13 _rot13 =  new Rot13();
+
+		[Fact]
+		public void Test1()
+		{
+			var result = _rot13.Method("ROT13 example.");
+			Assert.Equal("EBG13 rknzcyr.", result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			var result = _rot13.Method("This is my first ROT13 excercise!");
+			Assert.Equal("Guvf vf zl svefg EBG13 rkprepvfr!", result);
+		}
+	}
+}
