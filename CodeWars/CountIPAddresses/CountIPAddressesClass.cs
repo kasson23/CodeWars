c0ca48df20ec14ac9a0c4cd153038019b44fed29
Luke Kasson <luke.kasson@gmail.com>
2022-05-24 13:04:20 -0600

    newest

---
+++ CodeWars/CountIPAddresses/CountIPAddressesClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//CODEWARS link: https://www.codewars.com/kata/526989a41034285187000de4/train/csharp
+//INFO: https://docs.microsoft.com/en-us/troubleshoot/windows-client/networking/tcpip-addressing-and-subnetting
+namespace CodeWars.CountIPAddresses
+{
+    public class CountIPAddressesClass
+    {
+        public static long IpsBetween(string start, string end)
+        {
+            return -1;
+        }
+    }
+}
---
+++ CodeWars/CountIPAddresses/CountIPAddressesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeWars.CountIPAddresses
+{
+    public  class CountIPAddressesTests
+    {
+        [Fact]
+        public void SmapleTest()
+        {
+            Assert.Equal(50, CountIPAddressesClass.IpsBetween("10.0.0.0", "10.0.0.50"));
+            Assert.Equal(246, CountIPAddressesClass.IpsBetween("20.0.0.10", "20.0.1.0"));
+            Assert.Equal((1L << 32) - 1L, CountIPAddressesClass.IpsBetween("0.0.0.0", "255.255.255.255"));
+        }
+    }
+}
