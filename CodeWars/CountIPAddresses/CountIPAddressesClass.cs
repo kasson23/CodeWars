48ccb720c3fcde6246ed9e260bfff21fa7a40887
KingKass1! <luke.kasson@gmail.com>
2022-06-22 14:31:51 -0400

    don't feel like finishing

--- CodeWars/CountIPAddresses/CountIPAddressesClass.cs
+++ CodeWars/CountIPAddresses/CountIPAddressesClass.cs
@@ -11,9 +11,35 @@ namespace CodeWars.CountIPAddresses
 {
     public class CountIPAddressesClass
     {
+        //each point goes to 255 so just check if end is great then add diffencer, otherwise subtract differnac
         public static long IpsBetween(string start, string end)
         {
-            return -1;
+            var startSplit = start.Split(".").ToList();
+            var endSplit = end.Split(".").ToList();
+
+            var lengthOfIp= 3;
+            int totalDistance = 0;
+
+            for (int i = 0; i <= lengthOfIp; i++)
+            {
+                var endValue = int.Parse(endSplit[i]);
+                var startValue = int.Parse(startSplit[i]);
+                if (i != lengthOfIp)
+                {
+                    if(endValue != startValue)
+                    {
+                        var distance = endValue - startValue;
+
+                    }
+                }
+                else
+                {
+                    totalDistance += endValue - startValue;
+                }
+
+            }
+
+            return totalDistance;
         }
     }
 }
--- CodeWars/CountIPAddresses/CountIPAddressesTests.cs
+++ CodeWars/CountIPAddresses/CountIPAddressesTests.cs
@@ -13,7 +13,15 @@ namespace CodeWars.CountIPAddresses
         public void SmapleTest()
         {
             Assert.Equal(50, CountIPAddressesClass.IpsBetween("10.0.0.0", "10.0.0.50"));
+        }
+        [Fact]
+        public void SmapleTest02()
+        {
             Assert.Equal(246, CountIPAddressesClass.IpsBetween("20.0.0.10", "20.0.1.0"));
+        }
+        [Fact]
+        public void SmapleTest03()
+        {
             Assert.Equal((1L << 32) - 1L, CountIPAddressesClass.IpsBetween("0.0.0.0", "255.255.255.255"));
         }
     }
